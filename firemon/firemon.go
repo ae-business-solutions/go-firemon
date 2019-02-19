a486f38eb89f84964713690b15fa486f052142c0
Dan Placek <dplacek@gmail.com>
2019-02-19 16:16:08 -0600

    Add firemon/firemon.go

---
+++ firemon/firemon.go
@@ -0,0 +1,16 @@
+package firemon
+
+import "net/http"
+
+type Client struct {
+	httpClient *http.Client
+}
+
+func NewClient(httpClient *http.Client) *Client {
+	if httpClient == nil {
+		httpClient = http.DefaultClient
+	}
+	return &Client{
+		httpClient: httpClient,
+	}
+}
